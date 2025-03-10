3e3af164b16e889befff30eda8f7f33e0f42c8a5
Matheus Nascimento <matheusfla.12333@gmail.com>
2025-03-10 15:53:01 -0300

    feat:  ensure cart product quantity ca be increased

--- src/app/[slug]/menu/[productId]/components/cart-product-item.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-product-item.tsx
@@ -12,7 +12,8 @@ interface CartproductItemProps {
 }
 
 const CartProductItem = ({ product }: CartproductItemProps) => {
-  const { decreaseProductQuantity } = useContext(CartContext);
+  const { decreaseProductQuantity, increaseProductQuantity } =
+    useContext(CartContext);
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-3">
@@ -37,7 +38,11 @@ const CartProductItem = ({ product }: CartproductItemProps) => {
               <ChevronLeftIcon />
             </Button>
             <p className="w-7 text-xs">{product.quantity}</p>
-            <Button className="h-7 w-7 rounded-lg" variant="destructive">
+            <Button
+              className="h-7 w-7 rounded-lg"
+              variant="destructive"
+              onClick={() => increaseProductQuantity(product.id)}
+            >
               <ChevronRightIcon />
             </Button>
           </div>
